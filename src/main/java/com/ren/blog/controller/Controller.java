7d4b96456ab05dca0b3e2b160bba7f8674b20402
Renan Araújo <awkrenan@gmail.com>
2023-07-31 12:17:06 -0300

    GET posts: added get posts by id

--- src/main/java/com/ren/blog/controller/Controller.java
+++ src/main/java/com/ren/blog/controller/Controller.java
@@ -5,9 +5,13 @@ import com.ren.blog.posts.PostRepository;
 import com.ren.blog.posts.PostRequestDTO;
 import com.ren.blog.posts.PostResponseDTO;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +28,15 @@ public class Controller {
     List<PostResponseDTO> postList =
         postRepository.findAll().stream().map(PostResponseDTO::new).toList();
     return postList;
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Object> getSinglePost(@PathVariable(value = "id") Long id) {
+    Optional<Post> getPost = postRepository.findById(id);
+    if (getPost.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Post not found.");
+    }
+    return ResponseEntity.status(HttpStatus.OK).body(getPost.get());
   }
 
   @CrossOrigin(origins = "*", allowedHeaders = "*")
