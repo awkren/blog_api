561626270eb7a3f4c3deca7b557b815197cb53f6
Renan Araújo <awkrenan@gmail.com>
2023-07-31 12:01:17 -0300

    added POST new posts endpoint

--- src/main/java/com/ren/blog/controller/Controller.java
+++ src/main/java/com/ren/blog/controller/Controller.java
@@ -1,10 +1,15 @@
 package com.ren.blog.controller;
 
+import com.ren.blog.posts.Post;
 import com.ren.blog.posts.PostRepository;
+import com.ren.blog.posts.PostRequestDTO;
 import com.ren.blog.posts.PostResponseDTO;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("posts")
 public class Controller {
 
-  @Autowired
-  private PostRepository postRepository;
+  @Autowired private PostRepository postRepository;
 
   @GetMapping
   public List<PostResponseDTO> getPosts() {
-    List<PostResponseDTO> postList = postRepository.findAll().stream().map(PostResponseDTO::new).toList();
+    List<PostResponseDTO> postList =
+        postRepository.findAll().stream().map(PostResponseDTO::new).toList();
     return postList;
+  }
+
+  @CrossOrigin(origins = "*", allowedHeaders = "*")
+  @PostMapping
+  public void savePost(@RequestBody PostRequestDTO data) {
+    Post postData = new Post(data);
+    postRepository.save(postData);
   }
 }
--- src/main/java/com/ren/blog/posts/PostResponseDTO.java
+++ src/main/java/com/ren/blog/posts/PostResponseDTO.java
@@ -1,7 +1,9 @@
 package com.ren.blog.posts;
 
+
 public record PostResponseDTO(Long id, String title, String content) {
   public PostResponseDTO(Post post) {
     this(post.getId(), post.getTitle(), post.getContent());
   }
+
 }
