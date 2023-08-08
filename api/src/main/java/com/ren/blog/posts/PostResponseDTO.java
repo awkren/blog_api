1638820710bb852bf5f53e6e88ce9e95596f4210
Renan Araújo <awkrenan@gmail.com>
2023-08-08 17:33:02 -0300

    formatted timestamp to MMM DD YYYY

--- api/src/main/java/com/ren/blog/posts/PostResponseDTO.java
+++ api/src/main/java/com/ren/blog/posts/PostResponseDTO.java
@@ -1,9 +1,17 @@
 package com.ren.blog.posts;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public record PostResponseDTO(Long id, String title, String content, LocalDateTime createdAt) {
+
+  private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("MMM dd, yyyy");
+
   public PostResponseDTO(Post post) {
     this(post.getId(), post.getTitle(), post.getContent(), post.getCreatedAt());
+  }
+
+  public String getFormattedCreatedAt() {
+    return createdAt.format(FORMATTER);
   }
 }
--- front/src/app/model/Post.ts
+++ front/src/app/model/Post.ts
@@ -2,4 +2,6 @@ export class Post {
   id: number = 0;
   content: string = '';
   title: string = '';
+  createdAt: string = '';
+  formattedCreatedAt: string = '';
 }
